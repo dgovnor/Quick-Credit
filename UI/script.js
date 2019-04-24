20fb0b01c947598ce39870d4b4b2eaa4ec2014de
dgovnor <billyjude007@gmail.com>
2019-04-24 15:37:46 +0100

    arranged files in folder

---
+++ UI/script.js
@@ -0,0 +1,51 @@
+let menu = document.querySelector(".menu");
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  document.querySelector(".overlay").classList.toggle("menu-open");
+});
+
+let clicked = document.querySelectorAll(".nav a");
+clicked.forEach(clicked => {
+  clicked.onclick = () => {
+    document.querySelector(".menu").classList.remove("active");
+    document.querySelector(".overlay").classList.toggle("menu-open");
+  };
+});
+//signin
+let signinButton = document.querySelector("#signinButton");
+signinButton.addEventListener("click", () => {
+  document.getElementById("signinModal").style.display = "block";
+});
+
+let close = document.querySelector("#close");
+close.addEventListener("click", () => {
+  document.getElementById("signinModal").style.display = "none";
+});
+//signup
+
+let signupButton = document.querySelector("#signupButton");
+signupButton.addEventListener("click", () => {
+  document.getElementById("signupModal").style.display = "block";
+});
+
+let cancel = document.querySelector("#cancel");
+cancel.addEventListener("click", () => {
+  document.getElementById("signupModal").style.display = "none";
+});
+
+//Make passowrd visible
+let toggle = document.querySelector(".toggle-password");
+toggle.addEventListener("click", () => {
+  let changeToggle = toggle.classList.toggle("fa-eye-slash");
+  let input = document.querySelector(".input-field");
+  if (changeToggle) input.setAttribute("type", "text");
+  else input.setAttribute("type", "password");
+});
+
+let signupToggle = document.querySelector("#toggle-password");
+signupToggle.addEventListener("click", () => {
+  let changeToggle = signupToggle.classList.toggle("fa-eye-slash");
+  let input = document.querySelector("#input-field");
+  if (changeToggle) input.setAttribute("type", "text");
+  else input.setAttribute("type", "password");
+});
