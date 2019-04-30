7f9789c1f672c4448a89bdf1999a85f11488e420
dgovnor <billyjude007@gmail.com>
2019-04-30 08:58:17 +0100

    feature (signin-endpoint):add signin endpoint [finishes #165677407]

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,6 +41,30 @@ class UserController {
       data
     });
   }
+
+  static loginUser(req, res) {
+    const { email, password } = req.body;
+    const indexOfEmail = user.findIndex(user => user.email === email);
+
+    if (indexOfEmail !== -1) {
+      const checkPassword = authenticate.checkPassword(
+        password,
+        user[indexOfEmail].password
+      );
+      if (checkPassword) {
+        return res.status(200).send({
+          message: "Login Successful",
+          status: 200,
+          data: user[indexOfEmail]
+        });
+      }
+    }
+
+    return res.status(400).send({
+      status: 400,
+      error: "Email or password is incorrect"
+    });
+  }
 }
 
 export default UserController;
