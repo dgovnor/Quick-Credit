be4caabfa17b7fc0f1a342f5a80a05d70034c7f0
dgovnor <billyjude007@gmail.com>
2019-05-10 16:44:24 +0100

    feature(swagger): add swagger docs [Finishes #165939285]

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import homeRoute from './routes/homeRoute';
 import adminRoute from './routes/adminRoute';
 import userRoute from './routes/userRoute';
+import docs from './docs/swagger.json';
 
 // set up express app
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(docs));
 app.use('/', homeRoute);
 app.use('/api/v1', homeRoute);
 app.use('/api/v1/user', userRoute);
