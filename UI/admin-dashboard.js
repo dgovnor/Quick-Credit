a049a1f402c7f001fca83ab13947e570307827dc
dgovnor <billyjude007@gmail.com>
2019-05-15 13:45:28 +0100

    chore(refactor):admin dashboard [Finishes #165998091]

--- UI/admin-dashboard.js
+++ UI/admin-dashboard.js
@@ -1,84 +1,93 @@
-//functions
-const Modal = modal => {
-  let style = window.getComputedStyle(modal);
-  let display = style.getPropertyValue("display");
-  if (display === "none") {
-    modal.style.display = "block";
-  } else modal.style.display = "none";
+// functions
+const Modal = (modal) => {
+  const style = window.getComputedStyle(modal);
+  const display = style.getPropertyValue('display');
+  if (display === 'none') {
+    modal.style.display = 'block';
+  } else modal.style.display = 'none';
 };
 
-const closeWindow = modal => {
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+const closeWindow = (modal) => {
+  window.onclick = (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+    if (!event.target.matches('.dropbtn')) {
+      const dropdowns = document.getElementsByClassName('dropdown-content');
+      for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
     }
   };
 };
 
 const verifyAndApprove = (verifyTab, approveTab) => {
-  //check if verified is not selected
+  // check if verified is not selected
   if (verifyTab.checked == false) {
-    approveTab.removeAttribute("checked");
-    approveTab.setAttribute("disabled", "");
+    approveTab.removeAttribute('checked');
+    approveTab.setAttribute('disabled', '');
   }
-  //Add eventListner to all
-  verifyTab.addEventListener("click", () => {
-    verifyTab.toggleAttribute("checked");
+  // Add eventListner to all
+  verifyTab.addEventListener('click', () => {
+    verifyTab.toggleAttribute('checked');
 
     if (verifyTab.checked == false) {
-      approveTab.removeAttribute("checked");
-      approveTab.setAttribute("disabled", "");
+      approveTab.removeAttribute('checked');
+      approveTab.setAttribute('disabled', '');
       approveTab.checked = false;
     } else {
-      approveTab.removeAttribute("disabled");
+      approveTab.removeAttribute('disabled');
     }
   });
-  approveTab.addEventListener("click", () => {
-    approveTab.toggleAttribute("checked");
+  approveTab.addEventListener('click', () => {
+    approveTab.toggleAttribute('checked');
   });
 };
-//toggle hambuger
-let menu = document.querySelector(".menu");
+// toggle hambuger
+const menu = document.querySelector('.menu');
 
-menu.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  document.querySelector(".overlay").classList.toggle("menu-open");
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  document.querySelector('.overlay').classList.toggle('menu-open');
 });
-let viewTransactionModal = document.getElementById("viewTransactionModal");
-let viewTransactionButton = document.querySelector("#paymentInFavour");
-viewTransactionButton.addEventListener("click", () => {
-  viewTransactionModal.style.display = "block";
+const viewTransactionModal = document.getElementById('viewTransactionModal');
+const viewTransactionButton = document.querySelector('#paymentInFavour');
+viewTransactionButton.addEventListener('click', () => {
+  viewTransactionModal.style.display = 'block';
   closeWindow(viewTransactionModal);
 });
 
-//notification
-let notificationBox = document.querySelector("#notificationBox");
-let modalNotify = document.querySelector(".mobileNotify");
+// notification
+const notificationBox = document.querySelector('#notificationBox');
+const modalNotify = document.querySelector('.mobileNotify');
 closeWindow(modalNotify);
-notificationBox.addEventListener("click", () => {
+notificationBox.addEventListener('click', () => {
   Modal(modalNotify);
-  window.addEventListener("resize", () => {
-    modalNotify.style.display = "none";
+  window.addEventListener('resize', () => {
+    modalNotify.style.display = 'none';
   });
 });
 
-let emptyModal = document.getElementById("emptyModal");
-let table = document.getElementsByTagName("table")[0];
-let tbody = table.getElementsByTagName("tbody")[0];
-let modal = document.querySelector(".modal");
-let content = document.querySelector(".modal-content");
-tbody.onclick = function(e) {
+const emptyModal = document.getElementById('emptyModal');
+const table = document.getElementsByTagName('table')[0];
+const tbody = table.getElementsByTagName('tbody')[0];
+const modal = document.querySelector('.modal');
+const content = document.querySelector('.modal-content');
+tbody.onclick = function (e) {
   e = e || window.event;
 
-  var data = [];
-  var target = e.srcElement || e.target;
+  const data = [];
+  let target = e.srcElement || e.target;
 
-  while (target && target.nodeName !== "TR") {
+  while (target && target.nodeName !== 'TR') {
     target = target.parentNode;
   }
   if (target) {
-    var cells = target.getElementsByTagName("td");
-    for (var i = 0; i < cells.length; i++) {
+    const cells = target.getElementsByTagName('td');
+    for (let i = 0; i < cells.length; i++) {
       data.push(cells[i].innerHTML);
     }
   }
@@ -90,59 +99,87 @@ tbody.onclick = function(e) {
 
   closeWindow(emptyModal);
 };
-//current loans and repay modal
-let currentModal = document.querySelector("#currentModal");
-let viewCurrentButton = document.querySelector("#viewCurrentButton");
-let currentLoan = document.querySelector(".currentloan-tab");
-let repaidLoan = document.querySelector(".repaidloan-tab");
-let tabcurrent = document.querySelector("#tabcurrent");
-let tabrepaid = document.querySelector("#tabrepaid");
-let currentLoanContent = document.querySelector("#currentloan-tab-content");
-let repayLoanContent = document.querySelector("#repayloan-tab-content");
+// current loans and repay modal
+const currentModal = document.querySelector('#currentModal');
+const viewCurrentButton = document.querySelector('#viewCurrentButton');
+// const currentLoan = document.querySelector('.currentloan-tab');
+// const repaidLoan = document.querySelector('.repaidloan-tab');
+// const tabcurrent = document.querySelector('#tabcurrent');
+// const tabrepaid = document.querySelector('#tabrepaid');
+// const currentLoanContent = document.querySelector('#currentloan-tab-content');
+// const repayLoanContent = document.querySelector('#repayloan-tab-content');
 
 viewCurrentButton.onclick = () => {
   Modal(currentModal);
   closeWindow(currentModal);
 };
-currentLoan.addEventListener("click", () => {
-  if (currentLoanContent.style.display == "none") {
-    currentLoanContent.style.display = "block";
-    repayLoanContent.style.display = "none";
-    tabcurrent.classList.add("active");
-    tabrepaid.classList.remove("active");
-    currentLoanContent.classList.add("active");
-    repayLoanContent.classList.remove("active");
-  }
-});
-repaidLoan.addEventListener("click", () => {
-  if (currentLoanContent.style.display !== "none") {
-    currentLoanContent.style.display = "none";
-    repayLoanContent.style.display = "block";
-    tabcurrent.classList.remove("active");
-    tabrepaid.classList.add("active");
-    currentLoanContent.classList.remove("active");
-    repayLoanContent.classList.add("active");
-  }
-});
 
-let verify1 = document.getElementById("verify1");
-let approved1 = document.getElementById("approved1");
-let verify2 = document.getElementById("verify2");
-let approved2 = document.getElementById("approved2");
-let verify3 = document.getElementById("verify3");
-let approved3 = document.getElementById("approved3");
-let verify4 = document.getElementById("verify4");
-let approved4 = document.getElementById("approved4");
+const notRepaidLoan = () => {
+  // const input = document.getElementById('tabcurrent');
+  // const filter = input.value.toUpperCase();
+  const fullTable = document.getElementById('full-table');
+  const tr = fullTable.getElementsByTagName('tr');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < tr.length; i++) {
+    const td = tr[i].getElementsByTagName('td')[5];
+    if (td) {
+      if (td.innerHTML === 'Unpaid') {
+        tr[i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
+      }
+    }
+  }
+};
+const repaidLoan = () => {
+  const fullTable = document.getElementById('full-table');
+  const tr = fullTable.getElementsByTagName('tr');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < tr.length; i++) {
+    const td = tr[i].getElementsByTagName('td')[5];
+    if (td) {
+      if (td.innerHTML === '<span class="paid">Paid</span>') {
+        tr[i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
+      }
+    }
+  }
+};
+const currentLoan = () => {
+  const fullTable = document.getElementById('full-table');
+  const tr = fullTable.getElementsByTagName('tr');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < tr.length; i++) {
+    const td = tr[i].getElementsByTagName('td')[5];
+    if (td) {
+      tr[i].style.display = '';
+    }
+  }
+};
+const dropdown = () => {
+  document.getElementById('myDropdown').classList.toggle('show');
+};
+
+
+const verify1 = document.getElementById('verify1');
+const approved1 = document.getElementById('approved1');
+const verify2 = document.getElementById('verify2');
+const approved2 = document.getElementById('approved2');
+const verify3 = document.getElementById('verify3');
+const approved3 = document.getElementById('approved3');
+const verify4 = document.getElementById('verify4');
+const approved4 = document.getElementById('approved4');
 
 verifyAndApprove(verify1, approved1);
 verifyAndApprove(verify2, approved2);
 verifyAndApprove(verify3, approved3);
 verifyAndApprove(verify4, approved4);
 
-let signout = document.getElementById("signout");
-let signOutModal = document.getElementById("signOutModal");
+const signout = document.getElementById('signout');
+const signOutModal = document.getElementById('signOutModal');
 signout.onclick = () => {
   Modal(signOutModal);
-  document.querySelector(".menu").classList.remove("active");
-  document.querySelector(".overlay").classList.toggle("menu-open");
+  document.querySelector('.menu').classList.remove('active');
+  document.querySelector('.overlay').classList.toggle('menu-open');
 };
